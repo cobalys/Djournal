55d3b50b4cfd5197ac96829679365d4feb4404c2
Serge G. Spaolonzi <serge@cobalys.com>
2012-07-31 19:58:05 -0300

    Multiple Changes

---
+++ code/djournalsite/djournalsite/settings_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'djournal', 
+        'USER': 'root',
+        'PASSWORD': 'eagle785on3', 
+        'HOST': '',
+        'PORT': '', 
+    }
+}
